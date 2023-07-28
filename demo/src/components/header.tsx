203f1df24bdc99753ec09b951aca57664258e272
trekhleb <trehleb@gmail.com>
2023-07-28 21:59:27 +0200

    Do smth

--- demo/src/components/header.tsx
+++ demo/src/components/header.tsx
@@ -7,33 +7,49 @@ import {
 } from 'baseui/header-navigation'
 import { StyledLink } from 'baseui/link'
 import { AiFillGithub } from 'react-icons/ai'
+import { Notification } from 'baseui/notification'
 
 export function Header() {
   return (
-    <HeaderNavigation>
-      <StyledNavigationList $align={ALIGN.left}>
-        <StyledNavigationItem $style={{ fontWeight: 600 }}>
-          Micrograd TS
-        </StyledNavigationItem>
-      </StyledNavigationList>
-      <StyledNavigationList $align={ALIGN.center} />
-      <StyledNavigationList $align={ALIGN.right}>
-        <StyledNavigationItem
-          $style={{
-            alignItems: 'center',
-            justifyContent: 'center',
-            display: 'flex',
-            marginRight: '24px',
-          }}
-        >
-          <Block marginRight="5px" display="flex">
-            <AiFillGithub size={24} />
-          </Block>
-          <StyledLink href="https://github.com/trekhleb/micrograd-ts">
-            GitHub
-          </StyledLink>
-        </StyledNavigationItem>
-      </StyledNavigationList>
-    </HeaderNavigation>
+    <>
+      <HeaderNavigation>
+        <StyledNavigationList $align={ALIGN.left}>
+          <StyledNavigationItem $style={{ fontWeight: 600 }}>
+            Micrograd TS
+          </StyledNavigationItem>
+        </StyledNavigationList>
+        <StyledNavigationList $align={ALIGN.center} />
+        <StyledNavigationList $align={ALIGN.right}>
+          <StyledNavigationItem
+            $style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              display: 'flex',
+              marginRight: '24px',
+            }}
+          >
+            <Block marginRight="5px" display="flex">
+              <AiFillGithub size={24} />
+            </Block>
+            <StyledLink href="https://github.com/trekhleb/micrograd-ts">
+              GitHub
+            </StyledLink>
+          </StyledNavigationItem>
+        </StyledNavigationList>
+      </HeaderNavigation>
+
+      <Notification
+        overrides={{
+          Body: { style: { width: 'auto' } },
+        }}
+      >
+        A tiny scalar-valued autograd engine and a neural net on top of it{' '}
+        (TypeScript version of{' '}
+        <StyledLink href="https://github.com/karpathy/micrograd">
+          karpathy/micrograd
+        </StyledLink>{' '}
+        repo)
+      </Notification>
+    </>
   )
 }
