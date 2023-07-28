cceedb73233a52b295524ff8db2c25a5b4a63c65
trekhleb <trehleb@gmail.com>
2023-07-28 18:52:08 +0200

    Do smth

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,27 +1,46 @@
 import React from 'react'
-import { Tabs, Tab } from 'baseui/tabs-motion'
+import { Tabs, Tab, ORIENTATION } from 'baseui/tabs-motion'
 
 import { DemoMLP } from './demos/demo-mlp'
 import { DemoNeuron } from './demos/demo-neuron'
 import { DemoValues } from './demos/demo-values'
 import { Layout } from './layout'
 
+enum TabKey {
+  Value = 'value',
+  Neuron = 'neuron',
+  MLP = 'mlp',
+}
+
+type TabConfig = { title: string; content: React.ReactNode }
+
+const TabsMap: Map<TabKey, TabConfig> = new Map([
+  [TabKey.Value, { title: 'Value', content: <DemoValues /> }],
+  [TabKey.Neuron, { title: 'Neuron', content: <DemoNeuron /> }],
+  [TabKey.MLP, { title: 'MLP', content: <DemoMLP /> }],
+])
+
 export function App() {
-  const [activeKey, setActiveKey] = React.useState<string>('0')
+  const [activeKey, setActiveKey] = React.useState<React.Key>('0')
+
+  const tabs = Array.from(TabsMap.entries()).map(
+    ([tabKey, tabConfig]: [TabKey, TabConfig]) => (
+      <Tab title={tabConfig.title} key={tabKey}>
+        {tabConfig.content}
+      </Tab>
+    )
+  )
 
   return (
     <Layout>
       <Tabs
         activeKey={activeKey}
         onChange={({ activeKey }) => setActiveKey(`${activeKey}`)}
+        orientation={ORIENTATION.vertical}
         activateOnFocus
       >
-        <Tab title="Value"><DemoValues /></Tab>
-        <Tab title="Neuron"><DemoNeuron /></Tab>
-        <Tab title="MLP"><DemoMLP /></Tab>
+        {tabs}
       </Tabs>
-      
-      
     </Layout>
   )
 }
